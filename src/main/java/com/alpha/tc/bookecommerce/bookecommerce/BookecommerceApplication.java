13c58dabddd4aa1d2c9152954d739fa612b3332b
Tiago <tiagobhs@outlook.com>
2022-10-05 21:06:37 -0300

    Upload inicial de todos os arquivos do projeto

---
+++ src/main/java/com/alpha/tc/bookecommerce/bookecommerce/BookecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.alpha.tc.bookecommerce.bookecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alpha/tc/bookecommerce/bookecommerce/BookecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.tc.bookecommerce.bookecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
